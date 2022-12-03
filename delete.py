2e32cf25db11cf53c5e40adf9eb969756a1a392a
HCoder <86558426+HCoderHY@users.noreply.github.com>
2022-12-03 14:57:09 +0200

    Update delete.py

--- delete.py
+++ delete.py
@@ -3,11 +3,13 @@ import sys
 def foundMainFile():
     allFiles = os.listdir()
     for obj in allFiles:
+        #If you compiled this code in EXE, remove the line below, and insert: if (obj == "your_main_file(exe)_name.exe"):")
         if (obj == "main.py"):
             return True
     return False
 while True:
     if foundMainFile():
-        os.system("del delete.py")
+        #If you compiled this code in EXE, remove the line below, and insert: os.system("del your_main_file(exe)_name.exe")
+        os.system("del main.py")
         break
 sys.exit()
